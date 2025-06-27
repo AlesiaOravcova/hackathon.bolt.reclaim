c1a67defdaed76104912924096156eda5f7ac7f7
Christian Harries <primorialsoup@hotmail.com>
2025-06-27 17:40:18 +0100

    Updated OnboardingStep3.tsx

--- src/screens/Onboarding/OnboardingStep3.tsx
+++ src/screens/Onboarding/OnboardingStep3.tsx
@@ -71,7 +71,7 @@ export const OnboardingStep3 = (): JSX.Element => {
           transition={{ delay: 0.1 }}
           className="px-6 py-6"
         >
-          <h2 className="text-[22px] font-normal text-gray-900 mb-8 leading-[28px] tracking-[-0.45px]">
+          <h2 className="text-[22px] font-normal text-gray-900 mb-10 leading-[28px] tracking-[-0.45px]">
             Which activities help you feel most refreshed?
           </h2>
 
