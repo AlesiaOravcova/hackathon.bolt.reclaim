4bbb57addd35c566f7ef7ac3852625f8e5c41711
Christian Harries <primorialsoup@hotmail.com>
2025-06-27 17:40:27 +0100

    Updated OnboardingStep4.tsx

--- src/screens/Onboarding/OnboardingStep4.tsx
+++ src/screens/Onboarding/OnboardingStep4.tsx
@@ -58,7 +58,7 @@ export const OnboardingStep4 = (): JSX.Element => {
           transition={{ delay: 0.1 }}
           className="px-6 py-6"
         >
-          <h2 className="text-[22px] font-normal text-gray-900 mb-8 leading-[28px] tracking-[-0.45px]">
+          <h2 className="text-[22px] font-normal text-gray-900 mb-10 leading-[28px] tracking-[-0.45px]">
             When would you like gentle reminders when we find time for you?
           </h2>
 
