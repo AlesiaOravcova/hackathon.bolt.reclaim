6914ebd7d3a80088bec4621fe7a0d62b239e3abc
Christian Harries <primorialsoup@hotmail.com>
2025-06-30 19:25:52 +0100

    Fix spa button onClick handler

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -225,7 +225,7 @@ export const Dashboard = (): JSX.Element => {
                     </Button>
                     <Button
                       size="sm"
-                      onClick={https://www.spaexperience.org.uk/spa-treatments} to onClick={() => window.open('https://www.spaexperience.org.uk/spa-treatments', '_blank')}
+                      onClick={() => window.open('https://www.spaexperience.org.uk/spa-treatments', '_blank')}
                       className="bg-gradient-to-r from-pink-500 to-purple-600 text-white rounded-full px-4 py-2"
                     >
                       I'm In!
