1cd559f4eb7908a5db8fab2044ccf247d6b8363e
Christian Harries <primorialsoup@hotmail.com>
2025-06-30 11:30:39 +0100

    Updated Welcome.tsx

--- src/screens/Welcome/Welcome.tsx
+++ src/screens/Welcome/Welcome.tsx
@@ -51,6 +51,9 @@ export const Welcome = (): JSX.Element => {
         >
 
           <h1 className="text-3xl font-normal text-gray-900 leading-tight">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600">
+              Reclaim
+            </span>            
             your{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600">
               me time
