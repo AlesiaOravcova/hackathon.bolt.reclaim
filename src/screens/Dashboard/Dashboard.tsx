9013ee3d6063a4576eea4fdee6a76b927f878300
Christian Harries <primorialsoup@hotmail.com>
2025-06-30 19:24:15 +0100

    Updated Dashboard.tsx

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -225,7 +225,7 @@ export const Dashboard = (): JSX.Element => {
                     </Button>
                     <Button
                       size="sm"
-                      onClick={handleSpaCommit}
+                      onClick={https://www.spaexperience.org.uk/spa-treatments}
                       className="bg-gradient-to-r from-pink-500 to-purple-600 text-white rounded-full px-4 py-2"
                     >
                       I'm In!
