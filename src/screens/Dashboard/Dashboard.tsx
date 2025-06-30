5b2ec20fecc30a562896661f3f194ae387028bde
Christian Harries <primorialsoup@hotmail.com>
2025-06-30 16:01:14 +0100

    Updated Dashboard.tsx

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -42,7 +42,7 @@ export const Dashboard = (): JSX.Element => {
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
-          className="px-0 py-0"
+          className="px-6 py-0"
         >
           <div className="flex items-center justify-between mb-4">
             <div>
