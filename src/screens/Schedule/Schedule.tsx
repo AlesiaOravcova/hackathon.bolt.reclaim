f2a41addfc8f5515adba5b601e153bdc6d6bb3f9
hhyanng <hyang.uxuk@gmail.com>
2025-06-30 19:23:29 +0100

    Update Schedule Calendar Time Range to 1am-11pm

--- src/screens/Schedule/Schedule.tsx
+++ src/screens/Schedule/Schedule.tsx
@@ -80,11 +80,11 @@ export const Schedule = (): JSX.Element => {
 
   const currentWeek = getCurrentWeek();
 
-  // Extended time slots from 7am to 11pm
+  // Extended time slots from 1am to 11pm (22 hours total)
   const timeSlots = [
-    "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", 
-    "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", 
-    "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"
+    "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", 
+    "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", 
+    "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"
   ];
 
   const getEventsForDay = (dayIndex: number) => {
@@ -215,8 +215,8 @@ export const Schedule = (): JSX.Element => {
           </div>
         ))}
         
-        {/* Current time indicator (red line) - adjusted position for extended time range */}
-        <div className="absolute left-0 right-0 h-0.5 bg-red-500 z-20" style={{ top: '390px' }}>
+        {/* Current time indicator (red line) - adjusted position for 1am start */}
+        <div className="absolute left-0 right-0 h-0.5 bg-red-500 z-20" style={{ top: '774px' }}>
           <div className="w-3 h-3 bg-red-500 rounded-full -ml-1.5 -mt-1"></div>
         </div>
       </div>
