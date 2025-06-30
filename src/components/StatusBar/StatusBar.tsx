ba40e1e8a3d018432207b929d80322d35e2b2286
hhyanng <hyang.uxuk@gmail.com>
2025-06-30 21:10:57 +0100

    Updated StatusBar.tsx

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const StatusBar = (): JSX.Element => {
   return (
-    <div className="flex justify-between items-center px-6 pt-3 pb-1 text-black relative">
+    <div className="flex justify-between items-center px-6 pt-3 pb-1 text-black">
       <span className="text-sm font-semibold">8:23</span>
       <div className="flex items-center gap-1">
         <div className="flex gap-1">
@@ -14,15 +14,6 @@ export const StatusBar = (): JSX.Element => {
         <div className="ml-2 w-6 h-3 border border-black rounded-sm">
           <div className="w-4 h-1.5 bg-black rounded-sm m-0.5"></div>
         </div>
-      </div>
-      
-      {/* Bolt Badge */}
-      <div className="absolute top-8 right-6">
-        <img 
-          src="/white_circle_360x360.png" 
-          alt="Powered by Bolt" 
-          className="w-8 h-8 opacity-80 hover:opacity-100 transition-opacity duration-200"
-        />
       </div>
     </div>
   );
